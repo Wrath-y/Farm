c4de33344f8e3fff028a5fbda22200a2b75672ad
Wrath-y <805655528@qq.com>
2023-03-15 22:11:18 +0800

    feat

--- Assets/Editor/UIBuilder/ItemEditor.cs
+++ Assets/Editor/UIBuilder/ItemEditor.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
 
 
 public class ItemEditor : EditorWindow
 {
+    private ItemDataList_SO db;
+    private List<ItemDetails> itemList = new List<ItemDetails>();
+    private VisualTreeAsset itemRowTemplate;
+    private ListView itemListView;
+    
     [MenuItem("UIBuilder/ItemEditor")]
     public static void ShowExample()
     {
@@ -27,11 +33,55 @@ public class ItemEditor : EditorWindow
         VisualElement labelFromUXML = visualTree.Instantiate();
         root.Add(labelFromUXML);
         
-        // // A stylesheet can be added to a VisualElement.
-        // // The style will be applied to the VisualElement and all of its children.
-        // var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/UIBuilder/ItemEditor.uss");
-        // VisualElement labelWithStyle = new Label("Hello World! With Style");
-        // labelWithStyle.styleSheets.Add(styleSheet);
-        // root.Add(labelWithStyle);
+        itemRowTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UIBuilder/ItemRowTemplate.uxml");
+
+        itemListView = root.Q<VisualElement>("ItemList").Q<ListView>("ListView");
+        
+        LoadDB();
+        GenrateListView();
+    }
+
+    private void LoadDB()
+    {
+        var dataList = AssetDatabase.FindAssets("ItemDataList_SO");
+        if (dataList.Length == 0)
+        {
+            Debug.Log("ItemDataList_SO not find");
+            return;
+        }
+
+        var path = AssetDatabase.GUIDToAssetPath(dataList[0]);
+        db = AssetDatabase.LoadAssetAtPath<ItemDataList_SO>(path);
+
+        itemList = db.ItemDetailsList;
+        EditorUtility.SetDirty(db);
+        
+        Debug.Log(itemList[0].ItemId);
+    }
+
+    private void GenrateListView()
+    {
+        Func<VisualElement> makeItem = () => itemRowTemplate.CloneTree();
+
+        Action<VisualElement, int> bindItem = (e, i) =>
+        {
+            if (i > itemList.Count)
+            {
+                Debug.Log("i > itemList.Count");
+                return;
+            }
+
+            if (itemList[i].itemIcon == null)
+            {
+                Debug.Log("itemList[i].itemIcon is nil");
+                return;
+            }
+            e.Q<VisualElement>("Icon").style.backgroundImage = itemList[i].itemIcon.texture;
+            e.Q<Label>("Name").text = itemList[i] == null ? "item is nil" : itemList[i].itemName;
+        };
+
+        itemListView.itemsSource = itemList;
+        itemListView.makeItem = makeItem;
+        itemListView.bindItem = bindItem;
     }
 }
