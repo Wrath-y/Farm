cb24024db4c16ef3aff7c0551ed925c2a610915e
Wrath-y <805655528@qq.com>
2023-03-24 22:38:31 +0800

    feat

--- Assets/Scripts/Cursor/CursorManager.cs
+++ Assets/Scripts/Cursor/CursorManager.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite normal, tool, seed;
+
+    private Sprite _curSprite;
+    private Image _cursorImage;
+    private RectTransform _cursorCanvas;
+
+    private void Start()
     {
+        _cursorCanvas = GameObject.FindGameObjectWithTag("CursorCanvas").GetComponent<RectTransform>();
+        _cursorImage = _cursorCanvas.GetChild(0).GetComponent<Image>();
         
+        SetCursorImage(normal);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_cursorCanvas == null)
+        {
+            return;
+        }
+
+        _cursorImage.transform.position = Input.mousePosition;
+    }
+
+    private void SetCursorImage(Sprite sprite)
+    {
+        _cursorImage.sprite = sprite;
+        _cursorImage.color = new Color(1, 1, 1, 1);
     }
 }
