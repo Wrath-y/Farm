1ba6d7872827071dedd9895b960df237d5544267
Wrath-y <805655528@qq.com>
2023-04-27 19:39:08 +0800

    feat

---
+++ Assets/Scripts/Utils/SetCanvasSize.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCanvasSize : MonoBehaviour
+{
+    public Canvas[] canvasList;
+
+    void Start()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        float designWidth = 640; // 设计时使用的屏幕宽度
+        float designHeight = 360; // 设计时使用的屏幕高度
+        float ratio = Mathf.Min(screenWidth / designWidth, screenHeight / designHeight);
+        Debug.Log($"{Screen.width} {Screen.height} {ratio}");
+        foreach (Canvas canvas in canvasList)
+        {
+            canvas.scaleFactor = ratio;
+        }
+    }
+}
